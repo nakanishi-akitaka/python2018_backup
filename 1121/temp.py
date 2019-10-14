836f3a9ad3e216c80754acaf930d0986d2230570
NAKANISHI Akitaka <42338369+nakanishi-akitaka@users.noreply.github.com>
2019-10-14 17:41:00 +0900

    Add files via upload

---
+++ 1121/temp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 10:56:08 2018
+
+@author: Akitaka
+"""
+import numpy as np
+
+test = np.zeros((100,2))
+print(test[:,0])
+print(test[:,1])
+print(len(test),test.shape[0])
+
+
+
